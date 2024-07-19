2315201fea97ebb91c1b4c539e144a736aab7ca7
Christian Ibarbia <165136673+ChristianIbarbia@users.noreply.github.com>
2024-07-19 21:23:01 +0800

    Add files via upload

---
+++ CarShopApi/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using CarShopManagementServices;
+using CarshopManagementServices;
+
+namespace CarShopApi.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : Controller
+    {
+        UserGetServices _userGetServices;
+        UserTransactionServices _userTransactionServices;
+        public UserController()
+        {
+            _userGetServices = new UserGetServices();
+            _userTransactionServices = new UserTransactionServices();
+        }
+        [HttpGet]
+        public IEnumerable<CarShopApi.User> GetUsers()
+        {
+            var activeusers = _userGetServices.GetUsersByStatus(1);
+
+            List<CarShopApi.User> users = new List<User>();
+
+            foreach (var item in activeusers)
+            {
+                users.Add(new CarShopApi.User { brand = item.brand, model = item.model, status = item.status });
+
+
+            }
+            return users;
+        }
+        [HttpPost]
+        public JsonResult AddUser(User request)
+        {
+            var result = _userTransactionServices.CreateUser(request.brand, request.model);
+
+            return new JsonResult(result);
+        }
+
+
+
+
+
+        [HttpPatch]
+        public JsonResult UpdateUser(User request)
+        {
+            var result = _userTransactionServices.UpdateUser(request.brand, request.model);
+
+            return new JsonResult(result);
+        }
+
+        [HttpDelete]
+        public JsonResult DeleteUser(User request)
+        {
+            var result = _userTransactionServices.DeleteUser(request.brand, request.model);
+
+            return new JsonResult(result);
+
+        }
+    }
+}
+
+
+
---
+++ CarShopApi/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CarShopApi
+{
+    public class User
+    {
+        public string brand { get; set; }
+        public string model { get; set; }
+        public int status { get; set; }
+
+
+    }
+}
